f3e17053362f18602f98524eb37df7c1deb2022c
MrKadachigov <kadachigovs@gmail.com>
2016-12-12 14:54:12 +0200

    and ffffffff

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -2,40 +2,71 @@
 var header = $('header');
 var divheader = $('.header');
 var windowHeight = $(window).height();
+var scrol;
 
-
-var element = $('.sm-6');
-var element2 = $('.content-for-block');
-$(window).on('resize load', function () {
-    var elementWidth = element.width();
-    if(elementWidth < 992){
-        element2.css({'height': elementWidth/1});
-    }
-});
 
 $(window).on('scroll', function(){
     var scrollTop = $(window).scrollTop();
 
+    if(scrol > scrollTop){
+        header.addClass('header-down');
+        scrol = scrollTop;
+    }else {
+        header.removeClass('header-down');
+        scrol = scrollTop;
+    }
+
     if( scrollTop  > windowHeight/3){
+        header.addClass('fixed');
         divheader.addClass('is-fixed');
     } else{
         divheader.removeClass('is-fixed');
+        header.removeClass('fixed');
     }
 });
 
-//pursuingNav plugin revers scroll for header
-$(function(){
-    header.pursuingNav();
-});
-//slick plugin for slider
-$('.slider').slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    speed:1000,
-    dots: true,
-    arrows: false
-});
+(function ($){
+    $(window).on('load', function () {
+        var prev = $('.slick-prev');
+        var next = $('.slick-next');
 
-$(function(){
-    $('.nav-menu').slicknav();
-});
+        var parentCarousel, curentSlide, nextSlide, prevSlide;
+
+        function addClassSlider(self, elem){
+            parentCarousel = $(self).closest('.slider');
+            curentSlide =  $(parentCarousel).find('.slick-current');
+
+            nextSlide =  curentSlide.next('.slick-slide');
+            prevSlide =  curentSlide.prev('.slick-slide');
+
+
+            if(elem == 'next'){
+                $(curentSlide).addClass('translate-left');
+                $(nextSlide).addClass('translate-left');
+                $(prevSlide).addClass('translate-left');
+            }  else{
+                $(curentSlide).addClass('translate-right');
+                $(prevSlide).addClass('translate-right');
+                $(nextSlide).addClass('translate-right');
+            }
+
+        }
+
+        next.on('mouseover', function(){
+            addClassSlider(this, 'next');
+        });
+        next.on('mouseout', function () {
+            $(curentSlide).removeClass('translate-left');
+            $(nextSlide).removeClass('translate-left');
+            $(prevSlide).removeClass('translate-left');
+        });
+        prev.on('mouseover', function(){
+            addClassSlider(this, "prev");
+        });
+        prev.on('mouseout', function () {
+            $(curentSlide).removeClass('translate-right');
+            $(prevSlide).removeClass('translate-right');
+            $(nextSlide).removeClass('translate-right');
+        })
+    })
+})(jQuery);
